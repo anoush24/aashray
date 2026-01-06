b58c719196cd282aa482650fef218a8c1b42f2a8
Sushant <sushantnistane1@gmail.com>
2026-01-06 15:45:34 +0530

    Update dependencies and clean up route files

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,103 +1,103 @@
-const { Admin } = require("../models/admin.models.js");
-const bcrypt = require("bcryptjs");
-const { generateToken } = require("../middleware/generateToken.js");
-const {Blog} = require("../models/blogs.models.js")
+// const { Admin } = require("../models/admin.models.js");
+// const bcrypt = require("bcryptjs");
+// const { generateToken } = require("../middleware/generateToken.js");
+// const {Blog} = require("../models/blogs.models.js")
 
 
-// const regAdmin = async(req,res) => {
+// // const regAdmin = async(req,res) => {
+// //     try {
+// //         const {username, password} = req.body;
+
+// //         if (!username || !password) {
+// //             return res.status(400).json({ message: "Username and password are required" });
+// //         }
+
+// //         const exists = await Admin.findOne({username});
+// //         if(exists) {
+// //             return res.status(400).json({message:"Admin already exists"})
+// //         }
+
+// //         const salt = await bcrypt.genSalt(10);
+// //         const hashPass = await bcrypt.hash(password,salt)
+
+// //         const newAdmin = new Admin({
+// //             username,
+// //             password:hashPass
+// //         })
+// //         await newAdmin.save();
+
+// //         return res.status(201).json({alert:"admin registered success"})
+// //     }
+// //     catch(err) {
+// //         console.log(err);
+// //         return res.status(500).json({error:"Server error"})
+// //     }
+// // } 
+
+// const loginAdmin = async(req,res)=> {
 //     try {
-//         const {username, password} = req.body;
+//         const {username,password} = req.body;
 
-//         if (!username || !password) {
-//             return res.status(400).json({ message: "Username and password are required" });
+//         const admin = await Admin.findOne({username})
+//         if(!admin || !(await admin.comparePassword(password))) {
+//             res.status(400).json({message:"invalid cred.."})
 //         }
 
-//         const exists = await Admin.findOne({username});
-//         if(exists) {
-//             return res.status(400).json({message:"Admin already exists"})
+//         const payload = {
+//             id:admin.id,
+//             username:admin.username,
+//             role: "admin",
 //         }
-
-//         const salt = await bcrypt.genSalt(10);
-//         const hashPass = await bcrypt.hash(password,salt)
-
-//         const newAdmin = new Admin({
-//             username,
-//             password:hashPass
-//         })
-//         await newAdmin.save();
-
-//         return res.status(201).json({alert:"admin registered success"})
+//         const token = generateToken(payload)
+//         console.log("Admin token : ",token)
+//         res.json({message:"login success",token,role: "admin",})
 //     }
 //     catch(err) {
-//         console.log(err);
-//         return res.status(500).json({error:"Server error"})
+//         console.log(err)
+//         return res.status(500).json({error:"server error"})
 //     }
-// } 
+// }
 
-const loginAdmin = async(req,res)=> {
-    try {
-        const {username,password} = req.body;
+// const blogs = async(req,res) => {
+//     try {
+//         const blogs = await Blog.find({ isApproved: false });
+//         res.json(blogs);
+//     } catch (err) {
+//         res.status(500).json({ message: "Error fetching blogs",error:err.message });
+//     }
+// }
 
-        const admin = await Admin.findOne({username})
-        if(!admin || !(await admin.comparePassword(password))) {
-            res.status(400).json({message:"invalid cred.."})
-        }
+// const approve = async (req, res) => {
+//   const { id, action } = req.params;
 
-        const payload = {
-            id:admin.id,
-            username:admin.username,
-            role: "admin",
-        }
-        const token = generateToken(payload)
-        console.log("Admin token : ",token)
-        res.json({message:"login success",token,role: "admin",})
-    }
-    catch(err) {
-        console.log(err)
-        return res.status(500).json({error:"server error"})
-    }
-}
+//   try {
+//     if (!["approve", "reject"].includes(action)) {
+//       return res.status(400).json({ message: "Invalid action" });
+//     }
 
-const blogs = async(req,res) => {
-    try {
-        const blogs = await Blog.find({ isApproved: false });
-        res.json(blogs);
-    } catch (err) {
-        res.status(500).json({ message: "Error fetching blogs",error:err.message });
-    }
-}
+//     const blog = await Blog.findById(id);
+//     if (!blog) {
+//       return res.status(404).json({ message: "Blog not found" });
+//     }
 
-const approve = async (req, res) => {
-  const { id, action } = req.params;
+//     if (action === "approve") {
+//       blog.isApproved = true;
+//       await blog.save();
+//       return res.status(200).json({ message: "Blog approved", blog });
+//     }
 
-  try {
-    if (!["approve", "reject"].includes(action)) {
-      return res.status(400).json({ message: "Invalid action" });
-    }
+//     if (action === "reject") {
+//       await blog.deleteOne(); 
+//       return res.status(200).json({ message: "Blog rejected and deleted" });
+//     }
+//   } catch (err) {
+//     res.status(500).json({ message: "Error processing action", error: err.message });
+//   }
+// };
 
-    const blog = await Blog.findById(id);
-    if (!blog) {
-      return res.status(404).json({ message: "Blog not found" });
-    }
-
-    if (action === "approve") {
-      blog.isApproved = true;
-      await blog.save();
-      return res.status(200).json({ message: "Blog approved", blog });
-    }
-
-    if (action === "reject") {
-      await blog.deleteOne(); 
-      return res.status(200).json({ message: "Blog rejected and deleted" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: "Error processing action", error: err.message });
-  }
-};
-
-module.exports = {
-    regAdmin,
-    loginAdmin,
-    blogs,
-    approve,
-};
+// module.exports = {
+//     regAdmin,
+//     loginAdmin,
+//     blogs,
+//     approve,
+// };
